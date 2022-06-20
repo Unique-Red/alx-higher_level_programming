242727fb85e5e4f2abecb196907ca652d45f1111
root <noah13victor@gmail.com>
2022-06-20 09:31:59 -0700

    [fix] function that divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    #A function that divides element by element 2 lists
-    
+    # A function that divides element by element 2 lists
+
     temp_list = []
     for i in range(0, list_length):
         result = 0
