5f46d3ae591f35b8da79ede6bf51b5994bee6d9b
Unique-Red <vnoah410@gmail.com>
2022-09-05 16:05:49 -0700

    corrected the last string (JS)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 myArray.forEach((element) => console.log(element));
