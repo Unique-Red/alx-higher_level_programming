e4228b9b4a83c38941c87caa9ff86126dfef5ecb
Unique-Red <vnoah410@gmail.com>
2022-09-05 13:54:17 -0700

    fix: switched to forEach

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
-}
+myArray.forEach((element) => console.log(element));
